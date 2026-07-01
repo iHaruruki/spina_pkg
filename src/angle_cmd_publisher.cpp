e7027fe974a2c5dbcf52278eadbd87ef9cfaeb65
ryo.saegusa <ryo.saegusa@syblab.org>
2026-07-01 20:14:39 +0900

    chore:

--- src/angle_cmd_publisher.cpp
+++ src/angle_cmd_publisher.cpp
@@ -12,11 +12,21 @@ class AngleCmdPublisher : public rclcpp::Node
 public:
     AngleCmdPublisher()
         : Node("angle_cmd_publisher"), messages_(
-            {   "C2p-020", "C3p-020", "C4p-020", "C5p-020", "A0p-000", "A0r-000",
-                "C2r+020", "C3r+020", "C4r+020", "C5r+020", "A0p-000", "A0r-000",
-                "C2p+020", "C3p+020", "C4p+020", "C5p+020", "A0p-000", "A0r-000",
-                "C2r-020", "C3r-020", "C4r-020", "C5r-020", "A0p-000", "A0r-000",
-            }), current_index_(0)
+        {
+            "A0r-30", "A0r-29", "A0r-28", "A0r-27", "A0r-26", "A0r-25",
+            "A0r-24", "A0r-23", "A0r-22", "A0r-21", "A0r-20", "A0r-19",
+            "A0r-18", "A0r-17", "A0r-16", "A0r-15", "A0r-14", "A0r-13",
+            "A0r-12", "A0r-11", "A0r-10", "A0r-09", "A0r-08", "A0r-07",
+            "A0r-06", "A0r-05", "A0r-04", "A0r-03", "A0r-02", "A0r-01",
+            "A0r+00", "A0r+01", "A0r+02", "A0r+03", "A0r+04", "A0r+05",
+            "A0r+06", "A0r+07", "A0r+08", "A0r+09", "A0r+10", "A0r+11",
+            "A0r+12", "A0r+13", "A0r+14", "A0r+15", "A0r+16", "A0r+17",
+            "A0r+18", "A0r+19", "A0r+20", "A0r+21", "A0r+22", "A0r+23",
+            "A0r+24", "A0r+25", "A0r+26", "A0r+27", "A0r+28", "A0r+29",
+            "A0r+30",
+        }),
+        current_index_(0)
+
     {
         publisher_ = this->create_publisher<std_msgs::msg::String>("/angle_cmd", 10);
         timer_ = this->create_wall_timer(2s, std::bind(&AngleCmdPublisher::publish_message, this));
